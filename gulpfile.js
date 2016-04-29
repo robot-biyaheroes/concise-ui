3b526194fd405062dafc072f5c2635abef4671f7
volkovasystems <richeve.bebedor@gmail.com>
2016-04-29 17:05:35 +0800

    Fixed browser reloading.

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,70 @@
 "use strict";
 
-var gulp = require ('gulp');
-var flatten = require ('gulp-flatten');
-var plumber = require ('gulp-plumber');
-var sass = require ('gulp-sass');
-var rename = require ('gulp-rename');
-var browserSync = require('browser-sync');
-var nodemon = require('nodemon');
+var gulp = require ("gulp");
+var os = require( "os" );
+var flatten = require ("gulp-flatten");
+var plumber = require ("gulp-plumber");
+var sass = require ("gulp-sass");
+var rename = require ("gulp-rename");
+var browserSync = require("browser-sync");
+var nodemon = require("nodemon");
 
 
-gulp.task("default", ["browser-sync"]);
-gulp.task("build-sass", function buildSASS(){
+gulp.task( "default", [ "browser-sync" ] );
+gulp.task("build-sass",
+	function buildSASS( ){
 
-	return gulp.src ([
-		"bower_components/**/*.scss",
-		"client/**/*.scss"])
-	.pipe(plumber())
-	.pipe(flatten())
-	.pipe(sass())
-	.pipe(rename("concise.css"))
-	.pipe(gulp.dest("build"));
-});
+		return gulp
+			.src( [
+				"bower_components/bootstrap-sass/assets/stylesheets/**/*.scss",
+				"client/**/*.scss"
+			] )
+			.pipe( plumber( ) )
+			.pipe( flatten( ) )
+			.pipe( sass( ) )
+			.pipe( rename( "concise.css" ) )
+			.pipe( gulp.dest( "build" ) );
+	} );
 
 
-gulp.task('browser-sync', ['nodemon'], function(){
-	browserSync.init(null, {
-		proxy: "http://localhost:9000",
-	files: ["**/*.*"],
-	browser: "google chrome",
-	port: 8000,
-	});
-});
+gulp.task( "browser-sync", [ "nodemon" ], function( callback ){
+	browserSync
+		.init( null, {
+			"proxy": "http://localhost:9000",
+			"files": ["**/*.*"],
+			"port": 8000,
+			"ui": {
+				"port": 8001
+			},
+			"injectChanges": true,
+			"logLevel": "debug",
+			"browser": ( os.type( ) == "Linux" )? "google-chrome" : "google chrome"
+		} );
+} );
 
-gulp.task('nodemon', ['build-sass'], function(callback){
+
+gulp.task( "nodemon", function( callback ){
 	var started = false;
 
-	return nodemon ({
-		script: 'server.js'
-	}).on('start', function (){
-		if (!started) {
-			 callback();
-			 started = true;
-		}
-	});
+	return nodemon( {
+			"script": "./server.js",
+			"stdout": false
+		} )
+		.on( "start", function onStart( ){
+			if( !started ){
+				started = true;
 
-});
+				gulp.watch( "**/*.*", [ "build-sass" ] )
+					.on( "change", function onChange( ){
+						setTimeout( function onTimeout( ){
+							browserSync.reload( );
+						}, 1000 );
+					} );
+
+				callback( );
+			}
+		} )
+		.on( "error", function onError( ){
+
+		} );
+} );
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require("express");
 var path = require ("path");
-var APP = express();
+var APP = express( );
 
 APP.use (express.static(path.resolve("./build")));
 APP.use("/library", express.static(path.resolve("./bower_components")));
 APP.use(express.static(path.resolve("./")));
-APP.listen(9000);
+APP.listen( 9000 );
